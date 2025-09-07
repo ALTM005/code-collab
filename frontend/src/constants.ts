8ce7176c4369ba9b9f6001999a933be3e578008d
Aritha Munasinghe <arithaltm@gmail.com>
2025-09-06 17:24:34 -0700

    Added a constant file to hold the languages as language versions and code snippets for the editor as code snippets

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -6,3 +6,40 @@ export const LANGUAGE_VERSIONS = {
   csharp: "6.12.0",
   php: "8.2.3",
 };
+
+export const CODE_SNIPPETS = {
+  javascript: `function greet(name) {
+      console.log("Hello, " + name + "!");
+  }
+  greet("Alex");`,
+  typescript: `type Params = {
+      name: string;
+  }
+  function greet(data: Params) {
+      console.log("Hello, " + data.name + "!");
+  }
+  greet({ name: "Alex" });`,
+  python: `def greet(name):
+      print("Hello, " + name + "!")
+greet("Alex")`,
+  java: `public class HelloWorld {
+      public static void main(String[] args) {
+          System.out.println("Hello World");
+      }
+  }`,
+  csharp: `using System;
+  namespace HelloWorld
+  {
+      class Hello { 
+          static void Main(string[] args) {
+              Console.WriteLine("Hello World in C#");
+          }
+      }
+  }`,
+  php: `<?php
+  function greet($name) {
+      echo "Hello, " . $name . "!";
+  }
+  greet("World");
+  ?>`,
+};
